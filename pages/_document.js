b89efe120425e8fb20ad6023c383805571bd1e7e
tjsbear1111 <tjsbear1111@gmail.com>
2023-12-29 23:14:37 +0800

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -26,17 +26,6 @@ class MyDocument extends Document {
                       return <link key={index} rel="preload" href={fontUrl} as="font" type="font/woff2" />
                     }
                   })}
-
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-JN4B8H421G"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-JN4B8H421G');
-</script>
-
                 </Head>
 
                 <body className={`${BLOG.FONT_STYLE} font-light scroll-smooth`}>
