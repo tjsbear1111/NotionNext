19fa438bddc955a528b59b08282b978067b0e4a6
tjsbear1111 <tjsbear1111@gmail.com>
2023-12-29 22:42:23 +0800

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,50 +1,34 @@
-// eslint-disable-next-line @next/next/no-document-import-in-page
 import Document, { Html, Head, Main, NextScript } from 'next/document'
-import BLOG from '@/blog.config'
 
 class MyDocument extends Document {
-  static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx)
-    return { ...initialProps }
-  }
+    static async getInitialProps(ctx) {
+        const initialProps = await Document.getInitialProps(ctx)
+        return { ...initialProps }
+    }
 
-  render() {
-    return (
-            <Html lang={BLOG.LANG}>
+    render() {
+        return (
+            <Html>
                 <Head>
-                <link rel='icon' href= {`${BLOG.BLOG_FAVICON}`} />
-                  {/* 预加载字体 */}
-                  {BLOG.FONT_AWESOME && <>
-                      <link rel='preload' href={BLOG.FONT_AWESOME} as="style" crossOrigin="anonymous" />
-                      <link rel="stylesheet" href={BLOG.FONT_AWESOME} crossOrigin="anonymous" referrerPolicy="no-referrer" />
-                  </>}
-
-                  {BLOG.FONT_URL?.map((fontUrl, index) => {
-                    if (fontUrl.endsWith('.css')) {
-                      return <link key={index} rel="stylesheet" href={fontUrl} />
-                    } else {
-                      return <link key={index} rel="preload" href={fontUrl} as="font" type="font/woff2" />
-                    }
-                  })}
-
                     <script async src="https://www.googletagmanager.com/gtag/js?id=G-JN4B8H421G"></script>
-                    <script>
-                      window.dataLayer = window.dataLayer || [];
-                      function gtag(){dataLayer.push(arguments);}
-                      gtag('js', new Date());
+                    <script dangerouslySetInnerHTML={{
+                        __html:`
+                        window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+                      
+                        gtag('config', 'G-JN4B8H421G');
+                        `,                        
+                    }} />
 
-                      gtag('config', 'G-JN4B8H421G');
-                   </script>
-                     
                 </Head>
-
-                <body className={`${BLOG.FONT_STYLE} font-light scroll-smooth`}>
+                <body>
                     <Main />
                     <NextScript />
                 </body>
             </Html>
-    )
-  }
+        )
+    }
 }
 
 export default MyDocument
