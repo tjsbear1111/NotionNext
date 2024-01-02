c36c5e9e6ccee156de77dfbb26d35b88d3c7febe
tjsbear1111 <tjsbear1111@gmail.com>
2024-01-02 19:04:43 +0800

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -29,16 +29,6 @@ class MyDocument extends Document {
 
 <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5330018171034479"
      crossorigin="anonymous"></script>
-<!-- 1 -->
-<ins class="adsbygoogle"
-     style="display:block"
-     data-ad-client="ca-pub-5330018171034479"
-     data-ad-slot="1017300676"
-     data-ad-format="auto"
-     data-full-width-responsive="true"></ins>
-<script>
-     (adsbygoogle = window.adsbygoogle || []).push({});
-</script>
 
                 </Head>
 
