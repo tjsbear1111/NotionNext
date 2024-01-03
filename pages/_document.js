47b0e9ec8b91c73454d5daa37400160e1722d4f9
tjsbear1111 <tjsbear1111@gmail.com>
2024-01-03 22:06:42 +0800

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -33,20 +33,6 @@ class MyDocument extends Document {
                 </Head>
                 <body className={`${BLOG.FONT_STYLE} font-light scroll-smooth`}>
                     <Main />
-
-<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5330018171034479"
-     crossorigin="anonymous"></script>
-<!-- 3 -->
-<ins class="adsbygoogle"
-     style="display:block"
-     data-ad-client="ca-pub-5330018171034479"
-     data-ad-slot="1030685063"
-     data-ad-format="auto"
-     data-full-width-responsive="true"></ins>
-<script>
-     (adsbygoogle = window.adsbygoogle || []).push({});
-</script>
-       
                     <NextScript />
                 </body>
             </Html>
