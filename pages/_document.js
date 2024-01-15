1cdeec3f50e2d83ec64a8849e2525f02fe89c610
tjsbear1111 <tjsbear1111@gmail.com>
2024-01-15 21:43:42 +0800

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -29,7 +29,7 @@ class MyDocument extends Document {
 
 <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5330018171034479"
      crossorigin="anonymous"></script>
-
+       
                 </Head>
                 <body className={`${BLOG.FONT_STYLE} font-light scroll-smooth`}>
                     <Main />
